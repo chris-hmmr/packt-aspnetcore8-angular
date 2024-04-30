0338d794c1e27c27ee5c5ed192d6f8921b4f2ae7
Christopher Huemmer <christopherhuemmer@m3pro14.fritz.box>
2024-04-30 11:52:32 +0200

    chapter 03 - adds nav menu, routing, components
    added nav menu, routing, environment, new components for navigation, fetching data and home

--- Chapter_03/HealthCheck/healthcheck.client/src/app/app.component.ts
+++ Chapter_03/HealthCheck/healthcheck.client/src/app/app.component.ts
@@ -1,36 +1,13 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
-}
+import {HttpClient} from '@angular/common/http';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  public forecasts: WeatherForecast[] = [];
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.getForecasts();
-  }
-
-  getForecasts() {
-    this.http.get<WeatherForecast[]>('/api/weatherforecast').subscribe(
-      (result) => {
-        this.forecasts = result;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
+export class AppComponent {
+  constructor(private http: HttpClient) {
   }
 
   title = 'healthcheck.client';
--- Chapter_03/HealthCheck/healthcheck.client/src/app/app.module.ts
+++ Chapter_03/HealthCheck/healthcheck.client/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FetchDataComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
