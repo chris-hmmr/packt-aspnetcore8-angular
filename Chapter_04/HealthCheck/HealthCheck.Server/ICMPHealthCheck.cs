65cac1eb639e917e30b496e4af39bc6dcb01f210
Christopher Huemmer <christopherhuemmer@m3pro14.fritz.box>
2024-04-30 14:53:28 +0200

    chapter 04 - add json formatting for healthchecks

--- Chapter_04/HealthCheck/HealthCheck.Server/ICMPHealthCheck.cs
+++ Chapter_04/HealthCheck/HealthCheck.Server/ICMPHealthCheck.cs
@@ -5,8 +5,14 @@ namespace HealthCheck.Server;
 
 public class ICMPHealthCheck : IHealthCheck
 {
-    private readonly string Host = $"10.0.0.0";
-    private readonly int HealthyRoundtripTime = 300;
+    private readonly string Host;
+    private readonly int HealthyRoundtripTime;
+
+    public ICMPHealthCheck(string host, int healthyRoundtripTime)
+    {
+        Host = host;
+        HealthyRoundtripTime = healthyRoundtripTime;
+    }
     
     public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
     {
@@ -17,16 +23,19 @@ public class ICMPHealthCheck : IHealthCheck
             switch (reply.Status)
             {
                 case IPStatus.Success:
+                    var msg = $"ICMP to {Host} took {reply.RoundtripTime} ms";
                     return (reply.RoundtripTime > HealthyRoundtripTime)
-                        ? HealthCheckResult.Degraded()
-                        : HealthCheckResult.Healthy();
+                        ? HealthCheckResult.Degraded(msg)
+                        : HealthCheckResult.Healthy(msg);
                 default:
-                    return HealthCheckResult.Degraded();
+                    var err = $"ICMP to {Host} failed: {reply.Status}";
+                    return HealthCheckResult.Unhealthy(err);
             }
         }
         catch (Exception e)
         {
-            return HealthCheckResult.Unhealthy();
+            var err = $"ICMP failed: {e.Message}";
+            return HealthCheckResult.Unhealthy(err);
         }
     }
 }
--- Chapter_04/HealthCheck/HealthCheck.Server/Program.cs
+++ Chapter_04/HealthCheck/HealthCheck.Server/Program.cs
@@ -4,7 +4,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddHealthChecks()
-    .AddCheck<ICMPHealthCheck>("ICMP");
+    .AddCheck("ICMP_01",
+        new ICMPHealthCheck("www.ryadel.com", 100))
+    .AddCheck("ICMP_02",
+        new ICMPHealthCheck("www.google.com", 100))
+    .AddCheck("ICMP_03",
+        new ICMPHealthCheck($"www.{Guid.NewGuid():N}.com", 100));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -25,7 +30,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-app.UseHealthChecks(new PathString("/api/health"));
+app.UseHealthChecks(new PathString("/api/health"), new CustomHealthCheckOptions());
 
 app.MapControllers();
 
