b349ed3567aa2df510277f66efc7d49cb186fce1
Christopher Huemmer <christopherhuemmer@m3pro14.fritz.box>
2024-05-01 21:43:26 +0200

    chapter 05 - setup worldcities project

---
+++ Chapter_05/WorldCities/WorldCities.Server/ICMPHealthCheck.cs
@@ -0,0 +1,41 @@
+using System.Net.NetworkInformation;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace WorldCities.Server;
+
+public class ICMPHealthCheck : IHealthCheck
+{
+    private readonly string Host;
+    private readonly int HealthyRoundtripTime;
+
+    public ICMPHealthCheck(string host, int healthyRoundtripTime)
+    {
+        Host = host;
+        HealthyRoundtripTime = healthyRoundtripTime;
+    }
+    
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+    {
+        try
+        {
+            using var ping = new Ping();
+            var reply = await ping.SendPingAsync(Host);
+            switch (reply.Status)
+            {
+                case IPStatus.Success:
+                    var msg = $"ICMP to {Host} took {reply.RoundtripTime} ms";
+                    return (reply.RoundtripTime > HealthyRoundtripTime)
+                        ? HealthCheckResult.Degraded(msg)
+                        : HealthCheckResult.Healthy(msg);
+                default:
+                    var err = $"ICMP to {Host} failed: {reply.Status}";
+                    return HealthCheckResult.Unhealthy(err);
+            }
+        }
+        catch (Exception e)
+        {
+            var err = $"ICMP failed: {e.Message}";
+            return HealthCheckResult.Unhealthy(err);
+        }
+    }
+}
