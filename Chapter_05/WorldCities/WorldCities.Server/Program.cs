fb3d1b10681c089bdf20d80acfcaec14d462aef6
Christopher Huemmer <christopherhuemmer@m3pro14.local>
2024-05-08 15:57:12 +0200

    chapter 05 - add dbcontext and controllers

--- Chapter_05/WorldCities/WorldCities.Server/Program.cs
+++ Chapter_05/WorldCities/WorldCities.Server/Program.cs
@@ -1,4 +1,6 @@
 global using WorldCities.Server;
+using Microsoft.EntityFrameworkCore;
+using WorldCities.Server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +16,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add ApplicationDbContext and SQL Server support
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
